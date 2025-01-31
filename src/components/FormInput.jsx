a72c1e864383ff424caabf1ad04b32f856ed0f19
rojmaharjan <mhjroj78@gmail.com>
2025-01-31 17:47:02 +0545

    commit

--- src/components/FormInput.jsx
+++ src/components/FormInput.jsx
@@ -8,17 +8,17 @@ const FormInput = ({ icon, label, onChange, ...props }) => {
   };
 
   return (
-    <div className="input-wrapper relative w-full h-[54px] mb-6">
+    <div className="input-wrapper relative w-full h-[54px] mb-6 group">
       {/* Label for accessibility */}
       {label && <label className="block text-sm mb-2">{label}</label>}
 
-      <div className="absolute left-5 top-1/2 -translate-y-1/2 text-gray-400">
+      <div className="absolute left-5 top-1/2 -translate-y-1/2 text-gray-400 group-focus-within:text-[#3a91a5]">
         {icons[icon]}
       </div>
 
       <input
         {...props}
-        onChange={onChange} // Handle onChange passed down from parent
+        onChange={onChange}   
         className="input-field h-[42px] max-[768px]:h-full w-full outline-none text-base rounded border border-gray-300 px-12 transition-all duration-200 focus:border-[#3a91a5] placeholder:text-gray-400"
       />
     </div>
