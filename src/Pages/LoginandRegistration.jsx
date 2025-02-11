f79b7aebe6439c24fcd2dfcb3220273b4997c383
rojmaharjan <mhjroj78@gmail.com>
2025-02-11 11:30:26 +0545

    commit

--- src/Pages/LoginandRegistration.jsx
+++ src/Pages/LoginandRegistration.jsx
@@ -132,93 +132,11 @@ function LoginandRegistration() {
           )}
         </div>
 
-        {/* Sign Up Container */}
-        <div
-          className={`form-container sign-up-container ${
-            isSignUp ? "block" : "hidden"
-          } md:block md:absolute  md:h-full md:w-1/2 md:opacity-0 md:z-1 md:left-0 md:transition-all md:duration-600`}
-        >
-           <form
-            className="bg-[#F8FAFC] flex items-center  flex-col px-8 md:px-12 py-4 md:h-full text-center"
-            onSubmit={handleSubmit}
-          >
-            <h1 className="font-bold text-4xl ">Create Account</h1>
-            <SocialIcon />
-            <span className="text-l mb-4 ">
-              <p>or</p><p className="mt-2 mb-2">use your email for registration</p>
-            </span>
-            <FormInput
-              type="text"
-              name="name"
-              placeholder="Name"
-              icon="person"
-              value={formData.name}
-              onChange={handleInputChange}
-              required
-              title="Enter your name"
-            />
-            <FormInput
-              type="email"
-              name="email"
-              placeholder="Email"
-              icon="mail"
-              value={formData.email}
-              onChange={handleInputChange}
-              required
-              title="Enter your email address"
-            />
-            <FormInput
-              type="password"
-              name="password"
-              placeholder="Password"
-              icon="lock"
-              value={formData.password}
-              onChange={handleInputChange}
-              required
-              title='Enter new password'
-            />
-            {/* <button
-              type="submit"
-              className="mt-4 rounded-[20px] border border-[#3a91a5] bg-[#BAD8B6] text-white text-xs font-bold py-3 px-11 uppercase tracking-wider cursor-pointer transition-transform hover:opacity-90 active:scale-95"
-              disabled={loading}
-            >
-              {loading ? "Signing Up..." : "Sign Up"}
-            </button> */}
-             <button 
-             type="submit" className="relative flex items-center px-6 py-3 overflow-hidden text-xs transition-all bg-blue-500 rounded-[5px] group">
-              {/* <span className="absolute top-0 right-0 inline-block w-4 h-4 transition-all duration-500 ease-in-out bg-indigo-700 rounded group-hover:-mr-4 group-hover:-mt-4">
-                <span className="absolute top-0 right-0 w-5 h-5 rotate-45 translate-x-1/2 -translate-y-1/2 bg-white"></span>
-              </span>
-              <span className="absolute bottom-0 rotate-180 left-0 inline-block w-4 h-4 transition-all duration-500 ease-in-out bg-indigo-700 rounded group-hover:-ml-4 group-hover:-mb-4">
-                <span className="absolute top-0 right-0 w-5 h-5 rotate-45 translate-x-1/2 -translate-y-1/2 bg-white"></span>
-              </span> */}
-              <span className="absolute bottom-0 left-0 w-full h-full transition-all duration-500 ease-in-out delay-200 -translate-x-full bg-indigo-600 rounded-md group-hover:translate-x-0"></span>
-              <span className="relative w-full text-left text-white transition-colors duration-200 ease-in-out group-hover:text-white">
-              {loading ? "Signing Up..." : "Sign Up"}
-              </span>
-            </button>
-            <div className="mt-8 text-l text-gray-700 flex flex-col md:flex-row ">
-            <p>Already have an account?</p>
-            <Link
-              onClick={handleSignInClick}
-              label="Sign In" 
-              className="block ml-2 text-gray-700 hover:text-blue-500 hover:underline curser-pointer"
-            >
-               Sign In
-            </Link>  
-            </div>
-
-          </form>
-           
-         
-
-        </div>
-
-        {/* Sign In Container */}
+         {/* Sign In Container */}
         <div
           className={`form-container sign-in-container ${
             !isSignUp ? "block" : "hidden"
-          } md:block md:absolute md:top-0 md:h-full md:w-1/2 md:z-2 md:left-0 md:transition-all md:duration-600`}
+          } md:block md:absolute md:top-0 md:h-full md:w-1/2 md:z-2 md:right-0 md:transition-all md:duration-600`}
         >
         <form
             className="bg-[#F8FAFC] flex items-center  flex-col px-8 md:px-12 py-4 md:h-full text-center"
@@ -296,10 +214,90 @@ function LoginandRegistration() {
 
           </form>
           
+        </div> 
+        {/* Sign Up Container */}
+        <div
+          className={`form-container sign-up-container ${
+            isSignUp ? "block" : "hidden"
+          } md:block md:absolute  md:h-full md:w-1/2 md:opacity-0 md:z-1 md:left-0 md:transition-all md:duration-600`}
+        >
+           <form
+            className="bg-[#F8FAFC] flex items-center  flex-col px-8 md:px-12 py-4 md:h-full text-center"
+            onSubmit={handleSubmit}
+          >
+            <h1 className="font-bold text-4xl ">Create Account</h1>
+            <SocialIcon />
+            <span className="text-l mb-4 ">
+              <p>or</p><p className="mt-2 mb-2">use your email for registration</p>
+            </span>
+            <FormInput
+              type="text"
+              name="name"
+              placeholder="Name"
+              icon="person"
+              value={formData.name}
+              onChange={handleInputChange}
+              required
+              title="Enter your name"
+            />
+            <FormInput
+              type="email"
+              name="email"
+              placeholder="Email"
+              icon="mail"
+              value={formData.email}
+              onChange={handleInputChange}
+              required
+              title="Enter your email address"
+            />
+            <FormInput
+              type="password"
+              name="password"
+              placeholder="Password"
+              icon="lock"
+              value={formData.password}
+              onChange={handleInputChange}
+              required
+              title='Enter new password'
+            />
+            {/* <button
+              type="submit"
+              className="mt-4 rounded-[20px] border border-[#3a91a5] bg-[#BAD8B6] text-white text-xs font-bold py-3 px-11 uppercase tracking-wider cursor-pointer transition-transform hover:opacity-90 active:scale-95"
+              disabled={loading}
+            >
+              {loading ? "Signing Up..." : "Sign Up"}
+            </button> */}
+             <button 
+             type="submit" className="relative flex items-center px-6 py-3 overflow-hidden text-xs transition-all bg-blue-500 rounded-[5px] group">
+              {/* <span className="absolute top-0 right-0 inline-block w-4 h-4 transition-all duration-500 ease-in-out bg-indigo-700 rounded group-hover:-mr-4 group-hover:-mt-4">
+                <span className="absolute top-0 right-0 w-5 h-5 rotate-45 translate-x-1/2 -translate-y-1/2 bg-white"></span>
+              </span>
+              <span className="absolute bottom-0 rotate-180 left-0 inline-block w-4 h-4 transition-all duration-500 ease-in-out bg-indigo-700 rounded group-hover:-ml-4 group-hover:-mb-4">
+                <span className="absolute top-0 right-0 w-5 h-5 rotate-45 translate-x-1/2 -translate-y-1/2 bg-white"></span>
+              </span> */}
+              <span className="absolute bottom-0 left-0 w-full h-full transition-all duration-500 ease-in-out delay-200 -translate-x-full bg-indigo-600 rounded-md group-hover:translate-x-0"></span>
+              <span className="relative w-full text-left text-white transition-colors duration-200 ease-in-out group-hover:text-white">
+              {loading ? "Signing Up..." : "Sign Up"}
+              </span>
+            </button>
+            <div className="mt-8 text-l text-gray-700 flex flex-col md:flex-row ">
+            <p>Already have an account?</p>
+            <Link
+              onClick={handleSignInClick}
+              label="Sign In" 
+              className="block ml-2 text-gray-700 hover:text-blue-500 hover:underline curser-pointer"
+            >
+               Sign In
+            </Link>  
+            </div>
+
+          </form>
         </div>
 
+        
+
         {/* Desktop Overlay Container */}
-        <div className="hidden md:block overlay-container absolute top-0 left-1/2 w-1/2 h-full overflow-hidden transition-transform duration-600 z-100 " >
+        <div className="hidden md:block overlay-container absolute top-0 left-0 w-1/2 h-full overflow-hidden transition-transform duration-600 z-100 " >
           <div className="overlay  text-white relative -left-full h-full w-[200%] transform translate-x-0 transition-transform duration-600">
             <div className="overlay-panel overlay-left bg-[url('Pictures/register.png')]  bg-[#AAB99A]
              bg-contain bg-no-repeat bg-[length:90%] bg-center absolute flex items-center justify-center flex-col p-10 text-center top-0 h-full w-1/2 transform translate-x-0 transition-transform duration-600 -translate-x-[20%]">
